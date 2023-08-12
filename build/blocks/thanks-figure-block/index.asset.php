1242c0f2c30954f0978e9d09290609e625fd4c85
itmaroon <master@itmaroon.net>
2023-08-12 09:50:15 +0900

    8-12

--- build/blocks/thanks-figure-block/index.asset.php
+++ build/blocks/thanks-figure-block/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '61b621526808048a132f');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '76c7d2dcaa9da06d2e1d');
--- form-send-blocks.php
+++ form-send-blocks.php
@@ -17,8 +17,25 @@
 
 function itmar_form_send_blocks_block_init() {
 	foreach (glob(plugin_dir_path(__FILE__) . 'build/blocks/*') as $block) {
+		$block_name = basename($block);
+		$script_handle = 'form-handle-' . $block_name;
+		
+		// スクリプトの登録
+		wp_register_script(
+			$script_handle,
+			plugins_url( 'build/blocks/'.$block_name.'/index.js', __FILE__ ),
+			array( 'wp-blocks', 'wp-element', 'wp-i18n', 'wp-block-editor' )
+		);
 		// Static block
-		register_block_type($block);
+		register_block_type(
+			$block,
+			array(
+				'editor_script' => $script_handle
+			)
+		);
+		// その後、このハンドルを使用してスクリプトの翻訳をセット
+		wp_set_script_translations( $script_handle, 'form-send-blocks', plugin_dir_path( __FILE__ ) . 'languages' );
 	}
+	
 }
 add_action( 'init', 'itmar_form_send_blocks_block_init' );
